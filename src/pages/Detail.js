9f1eff703aa1b77e40d755e618e3cec5696d1bce
inbjork <emilioabautista@gmail.com>
2019-01-02 12:39:48 -0600

    react routes

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -1,36 +1,42 @@
-import React,{Component} from 'react'
+import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 
 const API_KEY = '151778c6'
 
-export class Detail extends Component{
-    static propTypes ={
-        id: PropTypes.string
-    }
-
-    state = {movie: {}}
-
-    _fetchMovie({id}){
-        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`)
-            .then(res => res.json())
-            .then(movie => {
-                console.log({movie})
-                this.setState({movie})
+export class Detail extends Component {
+    static propTypes = {
+        match: PropTypes.shape({
+            params: PropTypes.object,
+            isExact: PropTypes.bool,
+            path: PropTypes.string,
+            url: PropTypes.string
         })
     }
 
-    _goBack(){
+    state = { movie: {} }
+
+    _fetchMovie({ id }) {
+        fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&i=${id}`)
+            .then(res => res.json())
+            .then(movie => {
+                console.log({ movie })
+                this.setState({ movie })
+            })
+    }
+
+    _goBack() {
         window.history.back()
     }
 
-    componentDidMount(){
-        const { id } = this.props
-        this._fetchMovie({id})
+    componentDidMount() {
+        console.log(this.props)
+        const { id } = this.props.match.params
+        this._fetchMovie({ id })
     }
 
-    render(){
-        const{Title, Poster, Actors, Metascore, Plot} = this.state.movie
-        return(
+    render() {
+        const { Title, Poster, Actors, Metascore, Plot } = this.state.movie
+        return (
             <div>
                 <button onClick={this._goBack}>Volver</button>
                 <h1>{Title}</h1>
@@ -39,7 +45,7 @@ export class Detail extends Component{
                 <span>{Metascore}</span>
                 <p>{Plot}</p>
             </div>
-            
+
         )
     }
 }
